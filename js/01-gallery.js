b1dc567f88b898bea1dbbb058d1061d285de94e4
Anna Shalar <113612033+latikaann@users.noreply.github.com>
2023-02-26 01:26:40 +0200

    gallery ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+//
+//
+const gallery = document.querySelector(".gallery");
+const cardsMarkup = createCard(galleryItems);
+// console.log(createCard(galleryItems));
+
+gallery.insertAdjacentHTML("beforeend", cardsMarkup);
+
+gallery.addEventListener("click", onCardClick);
+
+function createCard(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+  </a>
+</div>
+`;
+    })
+    .join("");
+  return markup;
+}
+
+function onCardClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}">
+  `);
+
+  instance.show();
+
+  //   console.log(event.target);
+}
